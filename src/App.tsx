a47cb193320c7f03db93c3c97e7864635f5ffb10
vsyx <vsyxxio@gmail.com>
2022-01-14 20:14:29 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = (): React.ReactElement => {
+    return (
+        <div>Helloo</div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === 'development';
+    const port = (argv['PORT'] && parseInt(argv['PORT'])) || 3000;
+
+    return {
+        entry: './src/index.tsx',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        devtool: isDevelopment ? 'inline-source-map' : 'source-map',
+        devServer: {
+            host: '0.0.0.0',
+            hot: true,
+            port,
+        },
+        module: {
+            rules: [
+                { 
+                    test: /\.tsx?$/,
+                    include: path.join(__dirname, 'src'),
+                    use: [
+                        isDevelopment && {
+                            loader: 'babel-loader',
+                            options: { plugins: ['react-refresh/babel'] },
+                        },
+                        {
+                            loader: 'ts-loader',
+                            options: { transpileOnly: true },
+                        },
+                    ].filter(Boolean)
+                },
+                {
+                    test: /\.s[ac]ss|css$/,
+                    use: [
+                        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 2,
+                                modules: { auto: true }
+                            }
+                        },
+                        'postcss-loader',
+                        'sass-loader',
+                    ]
+                }
+            ],
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"]
+        },
+        plugins: [
+            isDevelopment && new ReactRefreshPlugin(),
+            new ForkTsCheckerWebpackPlugin(),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            })
+        ].filter(Boolean)
+    }
+}

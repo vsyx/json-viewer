fa445af12912f0d47f21ca4ab7587e6e216c5c5e
vsyx <vsyxxio@gmail.com>
2022-01-21 19:13:32 +0200

    Auto indent on paste

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -1,18 +1,40 @@
 import { setup } from './setup';
-import { EditorState, EditorStateConfig } from "@codemirror/state";
+import { EditorState, EditorStateConfig, Transaction, TransactionSpec } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
 import { json } from "@codemirror/lang-json";
 import { foldPlugin } from "./fold";
+import { getIndentUnit } from '@codemirror/language';
+import { indentRangeSpec } from './editorUtils';
 
 const maxDimensionsTheme = EditorView.theme({
     "&": { height: "100vh" },
     ".cm-scroller": { overflow: "auto" },
-    ".cm-content, .cm-gutter": {minHeight: "100vh"}
+    ".cm-content, .cm-gutter": { minHeight: "100vh" }
+});
+
+const indentOnPaste = EditorState.transactionFilter.of(tr => {
+    if (tr.annotation(Transaction.userEvent) != 'input.paste') {
+        return tr;
+    }
+
+    const indent = getIndentUnit(tr.startState);
+    const transactions: TransactionSpec[] = [];
+
+    tr.changes.iterChanges((from, to, txtFrom, txtTo, txt) => {
+        const changes = indentRangeSpec(txt.sliceString(txtFrom, txtTo), { from, to }, indent);
+        if (changes !== null) {
+            transactions.push({
+                changes
+            });
+        }
+    });
+
+    return !transactions.length ? tr : tr.startState.update(...transactions);
 });
 
 export function createJsonEditor(parent: Element | DocumentFragment, editorState: EditorStateConfig = {}) {
     const fullEditorStateConfig = Object.assign({
-        extensions: [setup, json(), maxDimensionsTheme, foldPlugin()],
+        extensions: [setup, json(), maxDimensionsTheme, foldPlugin(), indentOnPaste],
     }, editorState)
 
     const view = new EditorView({
